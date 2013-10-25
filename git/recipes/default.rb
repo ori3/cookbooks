5ccad00d075c3477bdc8a8bcb0cee4f4a5e0f25b
oriroi <vagrant@vagrant-debian-squeeze.vagrantup.com>
2013-10-25 17:17:46 +0100

    adjust recipt git

--- git/recipes/default.rb
+++ git/recipes/default.rb
@@ -7,14 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
+raise 'invalid parame node[:git][:user][:name]' if node[:git][:user][:name].empty?
+
 package "git" do
   action :install
 end
 
-cookbook_file "/home/#{node[:current_user]}/.gitconfig" do
+template "/home/#{node[:current_user]}/.gitconfig" do
   mode 0744
   owner "#{node[:current_user]}"
   group "#{node[:current_user]}"
   action :create
+  variables({
+      :user =>
+        {
+            :name => node[:git][:user][:name],
+            :email => node[:git][:user][:email],
+        }
+  })
 end
 
